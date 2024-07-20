a9ba5097e02ab6ddf4e63f46f33471cc8e837bd8
riku929hr <33782267+riku929hr@users.noreply.github.com>
2024-07-20 17:17:40 +0900

    finish writing echo command

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,28 +1,29 @@
-use clap::{App, Arg};
+use clap::{Arg, ArgAction, Command};
 
 fn main() {
-    let matches = App::new("echor")
+    let matches = Command::new("echor")
         .version("0.1.0")
-        .author("your name <yourname@example.com>")
-        .about("Rust echo")
+        .author("Your Name <yourname@example.com>")
+        .about("Rust version of echo")
         .arg(
-            Arg::with_name("text")
+            Arg::new("text")
                 .value_name("TEXT")
                 .help("Input text")
                 .required(true)
-                .min_values(1),
+                .num_args(1..),
         )
         .arg(
-            Arg::with_name("omit_newline")
-                .short("n")
-                .help("Do not print newline")
-                .takes_value(false),
+            Arg::new("omit_newline")
+                .short('n')
+                .action(ArgAction::SetTrue)
+                .help("Do not print newline"),
         )
         .get_matches();
 
-    let text = matches.values_of_lossy("text").unwrap();
-    let omit_newline = matches.is_present("omit_newline");
+    let text: Vec<String> = matches.get_many("text").unwrap().cloned().collect();
+    let omit_newline = matches.get_flag("omit_newline");
 
     let ending = if omit_newline { "" } else { "\n" };
+
     print!("{}{}", text.join(" "), ending);
 }
