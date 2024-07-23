7d28d28fe78717cec913485b6f73eb571021acab
riku929hr <33782267+riku929hr@users.noreply.github.com>
2024-07-23 17:32:06 +0900

    finish catr

--- catr/src/main.rs
+++ catr/src/main.rs
@@ -33,8 +33,47 @@ fn run(args: Args) -> Result<()> {
     for filename in args.files {
         match open(&filename) {
             Err(e) => eprintln!("Failed to open {filename}: {e}"),
-            Ok(_) => println!("Opened {filename}"),
+            Ok(content) => {
+                if args.number_lines {
+                    print_lines_with_number(content)?;
+                } else if args.number_nonblank_lines {
+                    print_lines_with_number_nonblank(content)?;
+                } else {
+                    print_lines_wo_number(content)?;
+                }
+            }
         }
+    }
+
+    Ok({})
+}
+
+fn print_lines_with_number(content: Box<dyn BufRead>) -> Result<()> {
+    for (i, line) in content.lines().enumerate() {
+        println!("{:6}\t{}", i + 1, line?);
+    }
+
+    Ok({})
+}
+
+fn print_lines_with_number_nonblank(content: Box<dyn BufRead>) -> Result<()> {
+    let mut line_number: i32 = 1;
+    for line in content.lines() {
+        let line = line?;
+        if line.is_empty() {
+            println!();
+        } else {
+            println!("{:6}\t{}", line_number, line);
+            line_number += 1;
+        }
+    }
+
+    Ok({})
+}
+
+fn print_lines_wo_number(content: Box<dyn BufRead>) -> Result<()> {
+    for line in content.lines() {
+        println!("{}", line?);
     }
 
     Ok({})
